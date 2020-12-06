d7f3c9eaedf2cd05a303b353c4a4acd000497b4e
MahAmorim <61316782+MahAmorim@users.noreply.github.com>
2020-12-06 11:53:54 -0300

    Update ValidarDataSessao.java

--- projeto/src/main/java/Strategy/ValidarDataSessao.java
+++ projeto/src/main/java/Strategy/ValidarDataSessao.java
@@ -4,16 +4,34 @@ import Dominio.EntidadeDominio;
 import Dominio.Sessao;
 import dao.SessaoDAO;
 import dao.IDAO;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class ValidarDataSessao implements IStrategy {
+
     public String processar(EntidadeDominio entidade) {
-        
-        Sessao sessao = (Sessao) entidade;
-        IDAO dao = new SessaoDAO();
-    
-        if(sessao.getDt_fim() <= sessao.getDt_incio()){
-            return "A data de finalização da sessao deve ser menor do que a data de inicio!";
+        Date dataInicial, dataFinal;
+        String strDataInicial = "14-01-2017";
+        String strDataFinal = "14-01-2017";
+        SimpleDateFormat sdf1 = new SimpleDateFormat("DD-MM-YYYY");
+        SimpleDateFormat sdf2 = new SimpleDateFormat("DD-MM-YYYYY");
+        try {
+
+            dataFinal = sdf1.parse(strDataFinal);
+            dataInicial = sdf2.parse(strDataInicial);
+            System.out.println(dataInicial.compareTo(dataFinal));
+            System.out.println(dataInicial.after(dataFinal));
+            if (dataInicial.compareTo(dataFinal) == 1) {
+                return "A data de finalização da sessao deve ser menor do que a data de inicio!";
+            }
+
+        } catch (ParseException ex) {
+            Logger.getLogger(ValidarDataSessao.class.getName()).log(Level.SEVERE, null, ex);
         }
         return null;
     }
-} 
+
+}
